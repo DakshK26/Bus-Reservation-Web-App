8feadc7ba94a171d6229ff85b5ba76c62a5b4f99
Daksh Khanna <115382295+DakshK26@users.noreply.github.com>
2023-01-03 21:17:48 -0500

    Update Bus

--- src/main/java/com/web/bus/Bus.java
+++ src/main/java/com/web/bus/Bus.java
@@ -26,6 +26,31 @@ public class Bus {
     private double startLng;
     private double endLat;
     private double endLng;
+    private double distance;
 
+    /*
+    Method to calculate distance between start and end
+     */
+    // add a method to calculate the distance between the start and end destinations
+    public void calculateDistance() {
+        final int EARTH_RADIUS = 6371; // radius of the Earth in kilometers
 
+        // Convert Distance to Radians
+        double startLatRadians = Math.toRadians(startLat);
+        double endLatRadians = Math.toRadians(endLat);
+        double latDeltaRadians = Math.toRadians(endLat - startLat);
+        double lngDeltaRadians = Math.toRadians(endLng - startLng);
+
+        // Use Haversine Formula
+        double a = Math.sin(latDeltaRadians / 2) * Math.sin(latDeltaRadians / 2) +
+                Math.cos(startLatRadians) * Math.cos(endLatRadians) *
+                        Math.sin(lngDeltaRadians / 2) * Math.sin(lngDeltaRadians / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // Set distance
+        this.distance = EARTH_RADIUS * c;
+    }
 }
+
+
+
