883284310cb2b3396f0d950ac0e6992314ab2364
Daksh Khanna <115382295+DakshK26@users.noreply.github.com>
2023-01-07 19:48:39 -0500

    Arrange Packages

---
+++ src/main/java/com/web/bus/LoginView.java
@@ -0,0 +1,42 @@
+package com.web.bus;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+@PageTitle("Login")
+@CssImport("./styles/views/login/login-view.css")
+public class LoginView extends Div {
+
+    public LoginView() {
+        setId("login-view"); // Set element ID
+        // Declare components
+        var username = new TextField("Username");
+        var password = new PasswordField("Password");
+        Button login;
+        Button register;
+        // Add components
+        add(
+                new H1("Welcome!"),
+                username,
+                password,
+                login = new Button("Login", event -> { // Login action event
+
+                }),
+                register = new Button("Register", event ->{ // Register action event
+                    UI.getCurrent().navigate("register"); // Send user to register route
+                })
+        );
+        // Change theme of buttons
+        login.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_CONTRAST);
+        register.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+    }
+}
