5633bfb6389d75c55fb7fc7df0d118759da027fc
Daksh Khanna <115382295+DakshK26@users.noreply.github.com>
2023-01-07 21:31:07 -0500

    Update Views

--- src/main/java/com/web/bus/views/LoginView.java
+++ src/main/java/com/web/bus/views/LoginView.java
@@ -1,16 +1,21 @@
-package com.web.bus;
+package com.web.bus.views;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-
+/*
+ * @author: Daksh & Ashwin
+ * Date: Jan. 2023
+ * Description: GUI for login, Initial user screen
+ * Method List: public static void main(String[] args)
+ */
 @Route("")
 @PageTitle("Login")
 @CssImport("./styles/views/login/login-view.css")
@@ -21,8 +26,7 @@ public class LoginView extends Div {
         // Declare components
         var username = new TextField("Username");
         var password = new PasswordField("Password");
-        Button login;
-        Button register;
+        Button login, register, companyRedirect;
         // Add components
         add(
                 new H1("Welcome!"),
@@ -33,10 +37,14 @@ public class LoginView extends Div {
                 }),
                 register = new Button("Register", event ->{ // Register action event
                     UI.getCurrent().navigate("register"); // Send user to register route
+                }),
+                companyRedirect = new Button("Company Sign In", event -> { // Company action event
+                    UI.getCurrent().navigate("company"); // Send user to register route
                 })
         );
         // Change theme of buttons
         login.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_CONTRAST);
         register.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        companyRedirect.addThemeVariants(ButtonVariant.LUMO_TERTIARY, ButtonVariant.LUMO_CONTRAST);
     }
 }
