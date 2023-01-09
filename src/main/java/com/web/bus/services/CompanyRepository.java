f63ee09e063f2b782c36fd782b707b5968fba95b
Daksh Khanna <115382295+DakshK26@users.noreply.github.com>
2023-01-09 14:51:08 -0500

    Update

--- src/main/java/com/web/bus/services/CompanyRepository.java
+++ src/main/java/com/web/bus/services/CompanyRepository.java
@@ -3,12 +3,14 @@ package com.web.bus.services;
 import com.web.bus.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
 
 /*
  * @author: Daksh & Ashwin
  * Date: Jan. 2023
  * Description: Interface to use Jpa Repository to save and retrieve companies
  */
+@Service
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     // Create new query to find company by name
     Company findByName(String name);
--- src/main/java/com/web/bus/services/UserRepository.java
+++ src/main/java/com/web/bus/services/UserRepository.java
@@ -2,13 +2,14 @@ package com.web.bus.services;
 
 import com.web.bus.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
 
 /*
  * @author: Daksh & Ashwin
  * Date: Jan. 2023
  * Description: Interface to use Jpa Repository to save and retrieve customers
  */
+@Service
 public interface UserRepository extends JpaRepository<Customer, Long> {
 
     // Create new query to find customer by name
