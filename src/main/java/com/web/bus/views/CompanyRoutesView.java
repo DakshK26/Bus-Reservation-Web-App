933146e99b5e92310fd4e152be66358c00e5ce62
AshwinSant5 <Ashwinsant5@gmail.com>
2023-01-22 18:23:03 -0500

    Fixed User Repo, Custom Methods need update

--- src/main/java/com/web/bus/views/CompanyRoutesView.java
+++ src/main/java/com/web/bus/views/CompanyRoutesView.java
@@ -19,6 +19,7 @@ import com.web.bus.records.Bus;
 import com.web.bus.records.BusList;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @Route(value = "companyRoutesView", layout = CompanyMainLayout.class)
@@ -28,7 +29,7 @@ public class CompanyRoutesView extends VerticalLayout {
     private TextField searchbar;
     private Select<String> select;
     private Grid<Bus> table;
-    private BusList busses;
+    private BusList buses, companyBuses;
 
     private List<Bus> busList;
 
@@ -36,30 +37,13 @@ public class CompanyRoutesView extends VerticalLayout {
         // Get company from session data
         Company customer = (Company) UI.getCurrent().getSession().getAttribute("company");
 
-       // busses = new BusList();
-        //Bus bus2 = new Bus("Parkway", "Vancouver", "Hamilton", "20");
-       // Bus bus3 = new Bus("International", "Brampton", "Regina", "15");
-        //Bus bus4 = new Bus("BooLoo", "Brampton", "Regina", "15");
-       // Bus bus5 = new Bus("International", "Brampton", "Regina", "15");
-       // Bus bus6 = new Bus("International", "Brampton", "Regina", "15");
-        //Bus bus7 = new Bus("International", "Brampton", "Regina", "15");
-         //Bus bus8 = new Bus("International", "Brampton", "Regina", "15");
-        //Bus bus9 = new Bus("International", "Brampton", "Regina", "15");
-       // Bus bus10 = new Bus("International", "Brampton", "Regina", "15");
+        buses = new BusList();
+        buses = buses.readFileMaster();
+        companyBuses = new BusList();
+        companyBuses = companyBuses.searchCompany(customer.getName(), buses);
 
-        // busses.insert(bus);
-      //  busses.insert(bus2);
-      //  busses.insert(bus3);
-       // busses.insert(bus4);
-       // busses.insert(bus5);
-       // busses.insert(bus6);
-       // busses.insert(bus7);
-        //busses.insert(bus8);
-       // busses.insert(bus9);
-        //busses.insert(bus10);
 
-        //busList = Arrays.asList(busses.getList());
-
+        busList = Arrays.asList(companyBuses.getList());
         select = new Select<>();
         select.setItems("Start Destination", "End Destination");
         select.setValue("Start Destination");
@@ -72,7 +56,7 @@ public class CompanyRoutesView extends VerticalLayout {
             UI.getCurrent().navigate("companyAddRouteView"); // Send user to register route
         });
         table = new Grid<>();
-        //table.setItems(busList);
+        table.setItems(busList);
         table.addColumn(Bus::getBusID).setHeader("CompanyName");
         table.addColumn(Bus::getStartDestination).setHeader("Start Destination");
         table.addColumn(Bus::getEndDestination).setHeader("End Destination");
