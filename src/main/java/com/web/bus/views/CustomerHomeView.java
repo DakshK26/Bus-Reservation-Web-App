9c6450bc798217be218709bd0b30c8e0b67a6a5c
AshwinSant5 <Ashwinsant5@gmail.com>
2023-01-22 19:42:11 -0500

    Fixed User Repo, Custom Methods need update

--- src/main/java/com/web/bus/views/CustomerHomeView.java
+++ src/main/java/com/web/bus/views/CustomerHomeView.java
@@ -1,6 +1,5 @@
 package com.web.bus.views;
 
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
@@ -8,69 +7,47 @@ import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.web.bus.entities.Customer;
 import com.web.bus.records.Bus;
 import com.web.bus.records.BusList;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
-@Route(value = "main", layout = CustomerMainLayout.class)
-@PageTitle("Customer Home")
+@Route(value = "customerHomeView", layout = CustomerMainLayout.class)
+@PageTitle("CustomerHome")
 public class CustomerHomeView extends VerticalLayout {
     private Button search, clear;
     private TextField searchbar;
     private Select<String> select;
     private Grid<Bus> table;
-    private BusList busses;
+    private BusList buses;
+
+    private List<Bus> busList;
 
     public CustomerHomeView () throws IOException {
-        // Get customer from session data
-        Customer customer = (Customer) UI.getCurrent().getSession().getAttribute("customer");
+        buses = new BusList();
+        buses = buses.readFileMaster();
 
-        //busses = new BusList();
-        //Bus bus = new Bus("Freeway", "Toronto", "Ottawa", "10");
-        //Bus bus2 = new Bus("Parkway", "Vancouver", "Hamilton", "20");
-       // Bus bus3 = new Bus("International", "Brampton", "Regina", "15");
-        //Bus bus4 = new Bus("BooLoo", "Brampton", "Regina", "15");
-       // Bus bus5 = new Bus("International", "Brampton", "Regina", "15");
-       // Bus bus6 = new Bus("International", "Brampton", "Regina", "15");
-        //Bus bus7 = new Bus("International", "Brampton", "Regina", "15");
-      //  Bus bus8 = new Bus("International", "Brampton", "Regina", "15");
-        //Bus bus9 = new Bus("International", "Brampton", "Regina", "15");
-       // Bus bus10 = new Bus("International", "Brampton", "Regina", "15");
-
-        // busses.insert(bus);
-      //  busses.insert(bus2);
-      //  busses.insert(bus3);
-       // busses.insert(bus4);
-       // busses.insert(bus5);
-       // busses.insert(bus6);
-       // busses.insert(bus7);
-        //busses.insert(bus8);
-       // busses.insert(bus9);
-        //busses.insert(bus10);
-
-        //busList = Arrays.asList(busses.getList());
+        busList = Arrays.asList(buses.getList());
 
         select = new Select<>();
-        select.setItems("Bus ID", "Start Destination", "End Destination");
-        select.setValue("Bus ID");
+        select.setItems("Company", "Start Destination", "End Destination");
+        select.setValue("Company");
         searchbar = new TextField();
         searchbar.setPlaceholder("Search Criteria");
         searchbar.setPrefixComponent(VaadinIcon.SEARCH.create());
         search = new Button("Search");
         clear = new Button("Clear Filters");
         table = new Grid<>();
-        //table.setItems(busList);
-      //  table.addColumn(Bus::getBusID).setHeader("Bus ID");
+        table.setItems(busList);
+        table.addColumn(Bus::getBusID).setHeader("Company");
         table.addColumn(Bus::getStartDestination).setHeader("Start Destination");
         table.addColumn(Bus::getEndDestination).setHeader("End Destination");
         table.addColumn(Bus::getDistance).setHeader("Distance");
@@ -79,6 +56,7 @@ public class CustomerHomeView extends VerticalLayout {
         table.addComponentColumn(item -> {
             return new Button("Book");
                 }).setHeader("Book Bus");
+       
 
         table.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
 
